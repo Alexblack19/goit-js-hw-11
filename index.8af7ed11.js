dc4517bb8fc155e652daa0714f68560378b8c92e
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-31 16:01:27 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-11@4a28b886d780a493d4e6fffa44b93d7ad66c65cc 🚀

---
+++ index.8af7ed11.js
@@ -0,0 +1,2 @@
+console.log("Hello"),console.log("Hello");
+//# sourceMappingURL=index.8af7ed11.js.map
---
+++ index.8bd6fdae.js
@@ -0,0 +1,2 @@
+console.log("Hello"),console.log("Hello");
+//# sourceMappingURL=index.8bd6fdae.js.map
