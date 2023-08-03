e95f43cc89bf2835e23c80130a45ce9ff5493ff5
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-03 22:42:36 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import { searchPhoto } from './js/photo-api.js';
 
 const formEl = document.querySelector('#search-form');
-
+const galleryListEl = document.querySelector('.gallery')
 // formEl.addEventListener('submit', onSearchPhotoSubmit);
 
+const photoName = 'cat';
 
-  searchPhoto()
-    .then(data => console.log(data.hits))
-    .catch(error => console.log(error.message));
+searchPhoto(photoName)
+  .then(data => {
+    const photoArr = data.hits;
+    const galleryMarkup = createImgGalleryMarkup(photoArr);
+    galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+  })
+  .catch(error => console.log(error.message));
 
 
+
+
+function createImgGalleryMarkup(photoArr) {
+  console.log(photoArr);
+  const markup = photoArr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+      </p>
+    </div>
+  </div>`;
+      }
+    )
+    .join('');
+
+}
--- src/js/photo-api.js
+++ src/js/photo-api.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 
 // axios.defaults.headers.common['key'] = '38572739-8ecec7d616fae8b4ce60f4b21';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38572739-8ecec7d616fae8b4ce60f4b21';
 
-export const searchPhoto = async () => {
+export const searchPhoto = async (photoName) => {
   const response = await axios.get(
-    'https://pixabay.com/api/?key=38572739-8ecec7d616fae8b4ce60f4b21&q=cat&image_type=photo&orientation =horizontal&safesearch =true&per_page=40'
+    `${BASE_URL}`,
+    {
+      params: {
+        key: `${API_KEY}`,
+        q: `${photoName}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+      },
+    }
+
+    // 'https://pixabay.com/api/?key=38572739-8ecec7d616fae8b4ce60f4b21&q=cat&image_type=photo&orientation =horizontal&safesearch =true&per_page=40'
   );
   console.log(response);
   return response.data;
