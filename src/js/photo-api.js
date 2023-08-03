3b6cf9edff127eeedc67df6123798d0048c66b46
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-03 23:20:28 +0300

    Update photo-api.js

--- src/js/photo-api.js
+++ src/js/photo-api.js
@@ -16,9 +16,7 @@ export const searchPhoto = async (photoName) => {
         safesearch: true,
         per_page: 40,
       },
-    }
-
-    // 'https://pixabay.com/api/?key=38572739-8ecec7d616fae8b4ce60f4b21&q=cat&image_type=photo&orientation =horizontal&safesearch =true&per_page=40'
+    }    
   );
   console.log(response);
   return response.data;
