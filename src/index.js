59586f26ddc1b34a00b900da2d0708e3f1a1b589
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-03 23:01:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import { searchPhoto } from './js/photo-api.js';
 
 const formEl = document.querySelector('#search-form');
-const galleryListEl = document.querySelector('.gallery')
+const galleryListEl = document.querySelector('.gallery');
 // formEl.addEventListener('submit', onSearchPhotoSubmit);
 
-const photoName = 'cat';
+const photoName = 'dog';
 
 searchPhoto(photoName)
   .then(data => {
     const photoArr = data.hits;
     const galleryMarkup = createImgGalleryMarkup(photoArr);
+    console.log(galleryMarkup);
     galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
   })
   .catch(error => console.log(error.message));
 
-
-
-
 function createImgGalleryMarkup(photoArr) {
-  console.log(photoArr);
-  const markup = photoArr
+  return photoArr
     .map(
       ({
         webformatURL,
@@ -29,26 +26,25 @@ function createImgGalleryMarkup(photoArr) {
         views,
         comments,
         downloads,
-      }) => {
+      }) => {        
         `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-      </p>
-    </div>
-  </div>`;
+           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+           <div class="info">
+                <p class="info-item">
+                    <b>Likes</b>
+                </p>
+                <p class="info-item">
+                    <b>Views</b>
+                </p>
+                <p class="info-item">
+                    <b>Comments</b>
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b>
+                </p>
+           </div>
+         </div>`;
       }
     )
     .join('');
-
 }
