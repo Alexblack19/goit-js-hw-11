176b51c847f61b45bfadd99e8c8696a2f2daaab3
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-06 00:15:42 +0300

    update

--- src/js/go_top.js
+++ src/js/go_top.js
@@ -1,24 +1,22 @@
-$(document).ready(function(){
-    $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
-  });
-  
-  $(function() {
-   $.fn.scrollToTop = function() {
-    $(this).hide().removeAttr("href");
-    if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+$(document).ready(function () {
+  $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
+});
+
+$(function () {
+  $.fn.scrollToTop = function () {
+    $(this).hide().removeAttr('href');
+    if ($(window).scrollTop() >= '250') $(this).fadeIn('slow');
     var scrollDiv = $(this);
-    $(window).scroll(function() {
-     if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
-     else $(scrollDiv).fadeIn("slow")
+    $(window).scroll(function () {
+      if ($(window).scrollTop() <= '250') $(scrollDiv).fadeOut('slow');
+      else $(scrollDiv).fadeIn('slow');
     });
-    $(this).click(function() {
-     $("html, body").animate({scrollTop: 0}, "slow")
-    })
-   }
-  });
-  
-  $(function() {
-   $("#go-top").scrollToTop();
-  });
-  
-  
+    $(this).click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+  };
+});
+
+$(function () {
+  $('#go-top').scrollToTop();
+});
