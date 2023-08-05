8ac8db99fcb96d338d6c24b336d7e4bff6dd8227
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-05 18:16:20 +0300

    update

--- src/js/markup-card.js
+++ src/js/markup-card.js
@@ -9,24 +9,24 @@ export function createGalleryMarkup(photoArr) {
           views,
           comments,
           downloads,
-        }) => `<a class="card-link" href="${largeImageURL}">
-               <div class="photo-card">
-                 <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                 <div class="info">
-                    <p class="info-item">
-                        <b>Likes ${likes}</b>
-                    </p>
-                    <p class="info-item">
-                        <b>Views ${views}</b>
-                    </p>
-                    <p class="info-item">
-                        <b>Comments ${comments}</b>
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads ${downloads}</b>
-                    </p>
-                 </div>
-               </div>
-              </a>`
-      ).join();
+        }) => `<div class="photo-card">
+                 <a class="card-link" href="${largeImageURL}">
+                    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes ${likes}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Views ${views}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Comments ${comments}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads ${downloads}</b>
+                        </p>
+                    </div>
+                 </a>
+               </div>`
+      ).join('');
   }
